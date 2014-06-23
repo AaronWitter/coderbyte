f3c9a723d087f9b4540f98a956c7bbfd27c65a5c
Megacito <Megacito@users.noreply.github.com>
2014-06-23 18:33:48 +0100

    Create 08checknums.py

---
+++ 08checknums.py
@@ -0,0 +1,13 @@
+def CheckNums(num1,num2): 
+  if num2 > num1:
+    return 'true'
+  elif num1 == num2:
+    return '-1'
+  else:
+    return 'false'
+
+    
+    
+# keep this function call here  
+# to see how to enter arguments in Python scroll down
+print CheckNums(raw_input())  
