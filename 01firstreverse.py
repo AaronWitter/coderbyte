495433cb099e00f4380a527fcc34c4c53fa9bfa7
Megacito <Megacito@users.noreply.github.com>
2014-06-23 18:27:34 +0100

    Create 01firstreverse.py

---
+++ 01firstreverse.py
@@ -0,0 +1,9 @@
+def FirstReverse(str): 
+  
+  str = str[::-1] 
+  return str 
+    
+    
+# keep this function call here  
+# to see how to enter arguments in Python scroll down
+print FirstReverse(raw_input())  
