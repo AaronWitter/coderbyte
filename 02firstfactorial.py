6f8b39dd7baa7eb73287933d993b64ef0da6896c
Megacito <Megacito@users.noreply.github.com>
2014-06-23 18:28:18 +0100

    Create 02firstfactorial.py

---
+++ 02firstfactorial.py
@@ -0,0 +1,11 @@
+import math
+
+def FirstFactorial(num): 
+  return num * math.factorial(num-1)
+    
+  
+    
+    
+# keep this function call here  
+# to see how to enter arguments in Python scroll down
+print FirstFactorial(raw_input())        
