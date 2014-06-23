702bf408378013b241fe3f2c7725d33778b904cb
Megacito <Megacito@users.noreply.github.com>
2014-06-23 18:30:34 +0100

    Create 05simpleadding.py

---
+++ 05simpleadding.py
@@ -0,0 +1,9 @@
+def SimpleAdding(num): 
+  y = range(1,num+1)
+  x = sum(y)
+  return x
+    
+    
+# keep this function call here  
+# to see how to enter arguments in Python scroll down
+print SimpleAdding(raw_input())   
